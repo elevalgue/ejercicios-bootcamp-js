bd6b40876caca5d3915c663a11bb2854f96f677f
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-08-07 18:30:06 +0200

    Add hotels & stars for rating

--- module-02/lab-02/app.js
+++ module-02/lab-02/app.js
@@ -2,24 +2,56 @@
 
 console.log('Are you ready for a good time? :)');
 
-// 1ST OBJETIVE
+// SELECTED HOTEL
 
-let hotel = {
-    name: 'Terramar',
-    location: 'Sitges',
-    img:'./img/hotel.svg', 
-}
+let hotels = {
+    Terramar: {
+        name: 'Terramar',
+        location: 'Sitges',
+        img: './img/hotel.svg',
+    },
+    Rosso: {
+        name: 'Rosso',
+        location: 'Almuñecar',
+        img: './img/hotel.svg',
+    },
+    Fireflies: {
+        name: 'Fireflies',
+        location: 'Potes',
+        img: './img/hotel.svg',
+    },
+};
 
-document.getElementById('name-hotel').innerHTML = 'Hotel ' + hotel.name;
-document.getElementById('location-hotel').innerHTML = 'Ubicado en ' + hotel.location;
-document.getElementById('img-hotel').src = hotel.img; 
+let selectedHotel = prompt('Indica el hotel sobre el que quiere hacer la reseña: Terramar, Rosso o Fireflies');
 
-// 2ND OBJETIVE
+document.getElementById('name-hotel').innerHTML =
+'Hotel ' + hotels[selectedHotel].name;
 
-let rating = prompt('Puntua este hotel del 1 al 5, please');
-document.getElementById('rating').innerHTML = rating + ' estrellas';
+document.getElementById('location-hotel').innerHTML = 'Ubicado en ' + hotels[selectedHotel].location;
 
-// 3RD OBJETIVE
+document.getElementById('img-hotel').src = hotels[selectedHotel].img
+
+// RATING
+
+let rating = prompt('Cúantas estrellas le das a este hotel: una, dos, tres, cuatro o cinco');
+
+let stars = {
+    una:
+    "<span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+    dos:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span>",
+    tres:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span>",
+    cuatro:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9734;</span>",
+    cinco:
+    "<span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span><span>&#9733;</span>",
+};
+
+document.getElementById('rating').innerHTML = stars[rating];
+
+// REVIEW
 
 let anonymousUser = confirm('¿Quieres que la reseña sea anónima')
 document.getElementById('anonymous').checked = anonymousUser; 
+
