8d79bea4bb8123c112bac561c52aa29fa6ff4d4c
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-08-12 15:28:13 +0200

    Clean

--- module-04/lab-04/js/app.js
+++ module-04/lab-04/js/app.js
@@ -4,42 +4,50 @@
 console.log('Are you ready for a good time? :))');
 
 // 1. Crear funciones con operaciones matemáticas y mostrar resultados en consola
-let inputNumber1 = document.querySelector('js-number1'); 
-let inputNumber2 = document.querySelector('js-number2');
+
+// SELECTORS
 
 const buttonAdd = document.querySelector('js-button-add'); 
 const buttonSubstract = document.querySelector('js-button-substract'); 
 const buttonmultiply = document.querySelector('js-button-multiply'); 
 const buttonDivide = document.querySelector('js-button-division');
 
-inputNumber1= 2;
-inputNumber2 = 3; 
+const result = document.querySelector('js-result');
 
-const handleAddition = (number1, number2) => {
-    let resultAdd = inputNumber1 + inputNumber2
-    console.log(resultAdd, 'maricarmen', inputNumber1, inputNumber2);
-}
+// GET INPUT VALUE
+const inputNumber1 = () => parseInt(document.querySelector('js-number1').value);
+const inputNumber2 = () => parseInt(document.querySelector('js-number2').value);
 
-const handleSubstraction = (number1 , number2) => {
-    let resultAdd = inputNumber1 - inputNumber2
-    console.log(resultAdd, 'maricarmen', inputNumber1, inputNumber2);
-}
+console.log('maricarmen', inputNumber1);
 
-const handleMultiplication = (number1, number2) => {
-  let resultAdd = inputNumber1 * inputNumber2
-    console.log(resultAdd, 'maricarmen', inputNumber1, inputNumber2);
-}
+// const add = (number1, number2) => {
+//     console.log(inputNumber1);
+//     return inputNumber1 + inputNumber2; 
+   
+// }
 
-const handleDivision = (number1, number2) => {
-    let resultAdd = inputNumber1 / inputNumber2
-    console.log(resultAdd, 'maricarmen', inputNumber1, inputNumber2);
-}
+// const add = (number1, number2) => {
+//     const firsNumber = parseInt(inputNumber1).value; 
+//     const secondNumber = parseInt(inputNumber2).value;
+//     console.log(resultAdd, 'maricarmen', inputNumber1, inputNumber2, firsNumber, secondNumber );
+//     console.log(resultAdd, 'maricarmen', inputNumber1, inputNumber2, firsNumber, secondNumber );
+//    return parseInt(firsNumber + secondNumber)
+// }
 
-handleAddition(inputNumber1, inputNumber2); 
-handleSubstraction(inputNumber1, inputNumber2); 
-handleMultiplication(inputNumber1, inputNumber2); 
-handleDivision(inputNumber1, inputNumber2); 
 
+
+// const divide = (number1, number2) => {
+//     let resultDivide = inputNumber1 / inputNumber2
+//     console.log(resultDivide, 'maricarmen', inputNumber1, inputNumber2);
+// }
+
+// add(inputNumber1, inputNumber2); 
+// substract(inputNumber1, inputNumber2); 
+// multiply(inputNumber1, inputNumber2); 
+// divide(inputNumber1, inputNumber2); 
+
+
+// buttonAdd.addEventListener('click', add); 
 // 2. Añadir inputs corresponientes a los operandos, o sea 2
 
 // 3. Añadir 4 botones: uno por operación 
