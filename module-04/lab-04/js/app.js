d304c008446ad9f1a15384960412e1e16f9dbf0f
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-08-12 18:28:07 +0200

    Create add function

--- module-04/lab-04/js/app.js
+++ module-04/lab-04/js/app.js
@@ -1,59 +1,24 @@
-
 'use strict';
 
-console.log('Are you ready for a good time? :))');
+console.log('Are you ready for a good time? :)');
 
-// 1. Crear funciones con operaciones matemáticas y mostrar resultados en consola
+const inputElement1 = document.querySelector('.js-number1');
 
-// SELECTORS
+const inputElement2 = document.querySelector('.js-number2');
 
-const buttonAdd = document.querySelector('js-button-add'); 
-const buttonSubstract = document.querySelector('js-button-substract'); 
-const buttonmultiply = document.querySelector('js-button-multiply'); 
-const buttonDivide = document.querySelector('js-button-division');
+const resultElement = document.querySelector('.js-number2');
 
-const result = document.querySelector('js-result');
+const add = function () {
+    let number1Value = inputElement1.value;
+    number1Value = parseInt(inputElement1); 
 
-// GET INPUT VALUE
-const inputNumber1 = () => parseInt(document.querySelector('js-number1').value);
-const inputNumber2 = () => parseInt(document.querySelector('js-number2').value);
+    let number2Value = inputElement2.value;
+    number2Value = parseInt(inputElement2);
 
-console.log('maricarmen', inputNumber1);
+    const resultValue = number1Value + number2Value
+    console.log(resultValue);
+}
 
-// const add = (number1, number2) => {
-//     console.log(inputNumber1);
-//     return inputNumber1 + inputNumber2; 
-   
-// }
+inputElement1.addEventListener('change', add);
 
-// const add = (number1, number2) => {
-//     const firsNumber = parseInt(inputNumber1).value; 
-//     const secondNumber = parseInt(inputNumber2).value;
-//     console.log(resultAdd, 'maricarmen', inputNumber1, inputNumber2, firsNumber, secondNumber );
-//     console.log(resultAdd, 'maricarmen', inputNumber1, inputNumber2, firsNumber, secondNumber );
-//    return parseInt(firsNumber + secondNumber)
-// }
-
-
-
-// const divide = (number1, number2) => {
-//     let resultDivide = inputNumber1 / inputNumber2
-//     console.log(resultDivide, 'maricarmen', inputNumber1, inputNumber2);
-// }
-
-// add(inputNumber1, inputNumber2); 
-// substract(inputNumber1, inputNumber2); 
-// multiply(inputNumber1, inputNumber2); 
-// divide(inputNumber1, inputNumber2); 
-
-
-// buttonAdd.addEventListener('click', add); 
-// 2. Añadir inputs corresponientes a los operandos, o sea 2
-
-// 3. Añadir 4 botones: uno por operación 
-
-// 4. Recoger los inpuits value y pasarlos por funciones 
-
-
-
-// 5. Pintar resultado en pantalla: generRLOS dinameicamente en js: xElement.innerHTML = numnber; en un div, span, p lo que vea
+inputElement2.addEventListener('change', add);
