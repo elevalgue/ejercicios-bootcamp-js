b326112527db3a3207f457b3af7550573b4f04b3
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-17 12:08:07 +0200

    Add new folder

--- module-04/lab-04/js/app.js
+++ module-04/lab-04/js/app.js
@@ -2,32 +2,64 @@
 
 console.log('Are you ready for a good time? :)');
 
-const inputElement1 = document.querySelector('.js-number1');
-const inputElement2 = document.querySelector('.js-number2');
-const resultElement = document.querySelector('.js-number2');
+// set value
+const inputElement1 = document.querySelector('·js-number1');
+const inputElement2 = document.querySelector('·js-number2');
 
-const buttonElement = document.querySelector('.js-button');
+const resultElement = document.querySelector('·js-result');
+const button = document.querySelector('·js-button'); 
 
+
+button.addEventListener('click', add);
+button.addEventListener('click', add);
+
+// get value
 const add = function () {
+    event.preventDefault; 
     let number1Value = inputElement1.value;
-    console.log('maricarmen', number1Value);
-    number1Value = parseInt(inputElement1); 
+
+    number1Value = parseInt(inputElement1);
+    
 
     let number2Value = inputElement2.value;
-    console.log('pepino', number2Value);
+
     number2Value = parseInt(inputElement2);
 
-    const resultValue = number1Value + number2Value
+
+    const resultValue = number1Value + number2Value;
+
     console.log(resultValue, 'holi');
 
-    if (isNaN(resultValue)) {
-        resultElement.classList.add('error-text')
-        resultElement.innerHTML = 'Corrige los datos'; 
-    } else {
-        resultElement.classList.remove('error-text')
-        resultElement.innerHTML = resultValue; 
-    }
+    // if (isNaN(resultValue)) {
+    //     resultElement.classList.add('error-text')
+    //     resultElement.innerHTML = 'Corrige los datos'; 
+    // } else {
+    //     resultElement.classList.remove('error-text')
+    //     resultElement.innerHTML = resultValue; 
+    // }
+
 }
 
-inputElement1.addEventListener('change', add);
-inputElement2.addEventListener('change', add);
+
+// const add = function () {
+//     let number1Value = inputElement1.value;
+//     console.log('maricarmen', number1Value);
+//     number1Value = parseInt(inputElement1); 
+
+//     let number2Value = inputElement2.value;
+//     console.log('pepino', number2Value);
+//     number2Value = parseInt(inputElement2);
+
+//     const resultValue = number1Value + number2Value
+//     console.log(resultValue, 'holi');
+
+//     // if (isNaN(resultValue)) {
+//     //     resultElement.classList.add('error-text')
+//     //     resultElement.innerHTML = 'Corrige los datos'; 
+//     // } else {
+//     //     resultElement.classList.remove('error-text')
+//     //     resultElement.innerHTML = resultValue; 
+//     // }
+// }
+
+
