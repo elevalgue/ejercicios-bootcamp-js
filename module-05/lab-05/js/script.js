b71ce33f0d09ffd4a436489fadffae2e9c246349
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-20 19:22:57 +0200

    Add task 2

--- module-05/lab-05/js/script.js
+++ module-05/lab-05/js/script.js
@@ -1,3 +1,50 @@
 'use strict'; 
 
 console.log('Are you ready for a good time? :)');
+
+const cart = [
+    {
+    id: 198752,
+    name: "Tinta DJ27 Color",
+    price: 52.95,
+    count: 3,
+    premium: true
+    },
+    {
+    id: 75621,
+    name: "Impresora ticketera PRO-201",
+    price: 32.75,
+    count: 2,
+    premium: true
+    },
+    {
+    id: 54657,
+    name: "Caja de rollos de papel para ticketera",
+    price: 5.95,
+    count: 3,
+    premium: false
+    },
+    {
+    id: 3143,
+    name: "Caja de folios DIN-A4 80gr",
+    price: 9.95,
+    count: 2,
+    premium: false
+    }
+]
+
+// 1. Mostrar el carrito de la ocompra
+console.log(cart);
+
+// 2. Listar productos
+for (let index = 0; index < cart.length; index++) {
+    displayCart(cart[index]);
+}
+
+function displayCart(product) {
+    console.log('--------------');
+    console.log('Producto: ' + product.id);
+    console.log('Precio:  ' + product.price);
+    console.log('Cantidad:  ' + product.count);
+    console.log('Es premium:  ' + product.premium);
+} 
