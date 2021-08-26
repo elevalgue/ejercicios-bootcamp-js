dfa303fcb4be4b31fb7c45e1957d851120b56148
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-26 14:49:00 +0200

    Clean

--- module-05/lab-05/js/script.js
+++ module-05/lab-05/js/script.js
@@ -55,27 +55,58 @@ function displayCart(product) {
 
 // 3. Eliminar el producto con id 54657 del carrito de la compra
 
-/**
- * Esta tarea está comentada para que en la siguiente aparezcan todos los productos originales del carrito de la compra
- */
 
-// let index = 0; 
+let index = 0; 
 
-// for (let index = 0; index < cart.length; index++) {
-//     let cartId = cart[index].id;
-//     if (cartId === 54657) {
-//         cart.splice(index, 1);
-//     }
-// }
+for (let index = 0; index < cart.length; index++) {
+    let cartId = cart[index].id;
+    if (cartId === 54657) {
+        cart.splice(index, 1);
+    }
+}
 
-// console.log(cart, 'tras splice');
+console.log(cart, 'tras splice');
 
 // 4. Calcular el total del carrito de la compra (el coste de una línea es precio * cantidad)
-console.log('T A S K  04');
+
+/**
+ * He vuelto a declarar el array con otro nombre para que no se vea alterado por el splice de la tarea anterior
+ */
+
+const shoppingCart = [
+    {
+    id: 198752,
+    name: "Tinta DJ27 Color",
+    price: 52.95,
+    count: 3,
+    premium: false,
+    },
+    {
+    id: 75621,
+    name: "Impresora ticketera PRO-201",
+    price: 32.75,
+    count: 2,
+    premium: true,
+    },
+    {
+    id: 54657,
+    name: "Caja de rollos de papel para ticketera",
+    price: 5.95,
+    count: 3,
+    premium: true,
+    },
+    {
+    id: 3143,
+    name: "Caja de folios DIN-A4 80gr",
+    price: 9.95,
+    count: 2,
+    premium: true,
+    }
+]
 
 let finalPrice = 0; 
 
-for (const product of cart) {
+for (const product of shoppingCart) {
     const productPrice = product.price * product.count;
     finalPrice += productPrice; 
 }
@@ -85,7 +116,7 @@ console.log('El precio total del carrito de la compra asciende a ' + finalPrice 
 // 5. Filtrar por los productos que sean prime
 console.log('T A S K  05');
 
-for (const product of cart) {
+for (const product of shoppingCart) {
     if (product.premium) {
         console.log('---------------------');
         console.log('Estos son los productos premium: ' + product.name);
@@ -97,11 +128,9 @@ for (const product of cart) {
  */
 
 // 6. Si todos los productos son prime mostrar un mensaje "Pedido sin gastos de envío", si no "Este pedido tiene gastos de envío".
-console.log('T A S K  06');
-
-function displayPremiumOrNot(cart) {
+function displayPremiumOrNot(shoppingCart) {
     let premium = true;
-    for (const product of cart) {
+    for (const product of shoppingCart) {
         if (product.premium === false) {
             return 'Este pedido tiene gastos de envío. 💰';
         } 
@@ -109,15 +138,15 @@ function displayPremiumOrNot(cart) {
     return 'Pedido sin gastos de envío 🚀';
 }
 
-console.log(displayPremiumOrNot(cart));
+console.log(displayPremiumOrNot(shoppingCart));
 
 // 7. Mostrar el carrito en un listado de html básico
-function displayCartList(cart) {
+function displayCartList(shoppingCart) {
    
     let bodyEl = document.querySelector('body');
     let codeHTML = ''; 
     
-    for ( const product of cart) {
+    for ( const product of shoppingCart) {
         codeHTML += `<div>`;
         codeHTML += `<ul>`;
         codeHTML += `<li>Nombre: ${product.name}</li>`;
@@ -136,14 +165,14 @@ function displayCartList(cart) {
     bodyEl.innerHTML = codeHTML;
 }
 
-displayCartList(cart); 
+displayCartList(shoppingCart); 
 
 // 8. Aplicar un descuento del 5% si la compra es mayor de 100 €.
 function applyDiscount() {
 
     let subTotal = 0;
     
-    for (const product of cart) {
+    for (const product of shoppingCart) {
         subTotal += product.count * product.price;
         console.log('ID: ' + product.id + ' - ' + 'PRECIO: '+ product.price + ' - ' + 'CANTIDAD:' + product.count + ' - ' + 'TOTAL: ' + subTotal);
     }
