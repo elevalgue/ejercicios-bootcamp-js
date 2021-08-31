a8aeea9fba1d06eae7515ee674870a0a48053d25
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-31 13:43:04 +0200

    Add data

--- module-06/lab-06-02/js/indexof.js
+++ module-06/lab-06-02/js/indexof.js
@@ -1,1 +1,13 @@
-'use strict'; 
+'use strict';
+
+console.log('Maricarmen');
+
+// const text = 'hello its me'; 
+
+const plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+const encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+const decryptedText = document.querySelector('#decrypted');
+const encryptedText = document.querySelector('#encrypted');
+const btnDecrypted = document.querySelector('#btn-decrypt');
+const btnEncrypted = document.querySelector('#btn-encrypt');
