226355e2c4c3724ec89193ba01f9c74cb9fc584f
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-31 12:42:43 +0200

    Transform character & message

--- module-06/lab-06-02/js/script2.js
+++ module-06/lab-06-02/js/script2.js
@@ -3,6 +3,8 @@
 console.log('Maricarmen');
 console.log('Descifrando a enigma');
 
+const text = 'hello its me'; 
+
 const plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
 const encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
 
@@ -21,6 +23,20 @@ function searchIndex(alphabet, character) {
     }
 }
 
-// function transformCharacter(sourceAlphabet, resultAlphabet, character) {
-    
-// }
+function transformCharacter(sourceAlphabet, resultAlphabet, character) {
+    const characterIndex = searchIndex(sourceAlphabet, character);
+
+    if (characterIndex === -1) {
+        return character
+    }
+    return resultAlphabet[characterIndex]; 
+}
+
+function transformMessage(message, sourceAlphabet, resultAlphabet) {
+    let result = '';
+    const messageLowerCase = message.toLowerCase();
+    for (const character of messageLowerCase) {
+        result = result + transformCharacter(sourceAlphabet, resultAlphabet, character)
+    }
+    return result; 
+}
