f0fc1399b3d34a3f5226907dcefd6999ced83a2a
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-31 12:30:04 +0200

    Search index

--- module-06/lab-06-02/js/script2.js
+++ module-06/lab-06-02/js/script2.js
@@ -1,6 +1,26 @@
 'use strict';
 
+console.log('Maricarmen');
 console.log('Descifrando a enigma');
 
 const plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
 const encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+const decryptedText = document.querySelector('#decrypted');
+const encryptedText = document.querySelector('#encrypted');
+const btnDecrypted = document.querySelector('#btn-decrypt');
+const btnEncrypted = document.querySelector('#btn-encrypt');
+
+function searchIndex(alphabet, character) {
+    for (let index = 0; index < alphabet.length; index++) {
+        if (alphabet[index] === character) {
+            return index
+        }
+        console.log('itero', alphabet);
+        return -1
+    }
+}
+
+// function transformCharacter(sourceAlphabet, resultAlphabet, character) {
+    
+// }
