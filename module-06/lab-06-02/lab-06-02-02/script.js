b7aa6a4915bbf039e2e3a79bff83733edd5dd227
elena <elena.valenzuela.guerrero@gmail.com>
2021-08-31 14:52:54 +0200

    Add random pick

--- module-06/lab-06-02/lab-06-02-02/script.js
+++ module-06/lab-06-02/lab-06-02-02/script.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('Are you ready for a good time? :)');
+console.log('Maricarmen :)');
+
+
+let numbers = [];
+
+const randomPick = (n, min, max) => {
+    const range = max - min + 1;
+    const getRandomNumber = () => Math.floor(Math.random() * range) + min;
+    
+    for (let index = 0; index < n; index++) {
+        numbers[index] = getRandomNumber(); 
+    }
+
+    for (let index = 0; index < n; index++) {
+        if (numbers[index] === numbers [index++]) {
+            numbers[index] = getRandomNumber(); 
+        }
+    }
+
+    return numbers
+}
+
+randomPick(10, 1, 100);
+console.log(randomPick());
