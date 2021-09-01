f7e6d555a579e924987727625970f9463e33073a
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-01 18:08:42 +0200

    Calculate room price input value

--- module-04/lab-04-extra/02/js/script.js
+++ module-04/lab-04-extra/02/js/script.js
@@ -2,26 +2,65 @@
 
 console.log('Are you ready for a good time? :))');
 
-const btnEl = document.querySelector('#js-btn');
 const selectRoomType = document.querySelector('#js-room-type');
 const selectRoomSize = document.querySelector('#js-room-size');
+const inputSpa = document.querySelector('#js-checkbox-spa');
 const inputNights = document.querySelector('#js-input-nights');
 const inputParking = document.querySelector('#js-input-parking');
+const btnCalculate = document.querySelector('#js-btn');
+const spanFinalPrice = document.querySelector('#js-final-price');
 
 
-const stardard = 100,
-    junior = 120,
-    suite = 150;
+// const nightPrice = {
+//     stardard: 100,
+//     junior: 120,
+//     suite: 150
+// }
     
-const spa = 20;
-const parking = 10;
+const spaPrice = 20;
+const parkingPrice = 10;
+
+const fee = {
+    single: 0.75,
+    double: 1,
+    single: 1.25,
+}
 
 //1. Calcular el coste total de la estancia
 //  - Cosas a tener en cuenta:
 //      a. el SPA aumenta la tarifa en 20€
-//      b. la ocupación INDIVIDUAL reduce la tarifa el 25%
-//      c. el DOBLE la deja igual
-//      d. la TRIPLE incrementa un 25%
+//      b. la ocupación INDIVIDUAL reduce la tarifa el 25% ---- 0.75
+//      c. el DOBLE la deja igual ---- 1
+//      d. la TRIPLE incrementa un 25% ---- 1,25
 //      e. la noche de parking suma 10€
 
 // 2. Como no tengo ningún objeto que recorrer, el precio final tendré que calcularlo acorde a lo que introduzca el usuario en el input
+
+/*----- 1. CALCULATE ROOM PRICE -----*/
+
+function calculateRoomPrice() {
+    const roomType = selectRoomType.value;
+    console.log(roomType, 'maricarmen');
+    let price = 0;
+
+    if (roomType === 'standard') {
+        price = 100;
+        console.log(roomType, 'derby');
+    } else if (roomType === 'junior') {
+        price = 120;
+        console.log(roomType, 'motoretas');
+
+    } else{
+        price = 150;
+        console.log(roomType, 'burrito');
+
+    }
+    console.log(price);
+    return price; 
+
+}
+
+calculateRoomPrice();
+
+// Listeners
+selectRoomType.addEventListener('change', calculateRoomPrice)
