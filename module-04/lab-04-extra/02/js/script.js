3218f58624637781a17c550a744bdd2a5165e6e2
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-01 13:51:09 +0200

    Declare variables

--- module-04/lab-04-extra/02/js/script.js
+++ module-04/lab-04-extra/02/js/script.js
@@ -1,3 +1,17 @@
 'use strict';
 
 console.log('Are you ready for a good time? :))');
+
+const btnEl = document.querySelector('#js-btn');
+const selectRoomType = document.querySelector('#js-room-type');
+const selectRoomSize = document.querySelector('#js-room-size');
+const inputNights = document.querySelector('#js-input-nights');
+const inputParking = document.querySelector('#js-input-parking');
+
+
+const stardard = 100,
+    junior = 120,
+    suite = 150;
+    
+const spa = 20;
+const parking = 10; 
