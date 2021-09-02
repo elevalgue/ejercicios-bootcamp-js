8cbdabcda1a26c88a05e07b1dd4a1c198c98ba9f
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-02 16:51:35 +0200

    Add clear

--- module-04/lab-04-extra/01/js/app.js
+++ module-04/lab-04-extra/01/js/app.js
@@ -14,51 +14,14 @@ const getInputValue = () => parseInt(document.querySelector('#js-inputNumber').v
 let result = 0; 
 
 // 2. Math operations
-// const add = () => {
-//     if (Boolean(getInputValue()) + result) {
-//         console.log(typeof getInputValue(), typeof result, 'maricarmen');
-//         return getInputValue() + result;  
-//     } else {
-//         return "Error"; 
-//     }
-// }
+const
 
-const add = () => {
-    if (result === null) {
-        result = getInputValue();
-        console.log(result, 'hello');
-        console.log(getInputValue, 'hello there');
-    } else {
-        result = result + getInputValue()
-    }
+// 3. Clear
 
-    return result; 
+const clear = () => {
+    document.querySelector('#js-inputNumber').value = '';
+    document.querySelector('#js-result').innerHTML= '';
 }
 
-// const substract = () => {
-//     if (Boolean(getInputValueA()) && Boolean(getInputValueB())) {
-//         return getInputValueA() - getInputValueB() ;  
-//     } else {
-//         return "Error"; 
-//     }
-// }
-
-// 3. Display results
-const renderAdd = () => resultEl.innerHTML = add();
-const finalResult = () => document
-
-// const renderSubst = () => resultEl.innerHTML = substract();
-
-// const renderClear = () => resultEl.innerHTML = clear();
-
-// 4. Clear
-// const clear = () => {
-//     document.querySelector('#js-inputNumberA').value = ''; 
-//     document.querySelector('#js-result').innerHTML= '';
-// }
-
 // LISTENERS
-buttonAdd.addEventListener('click', renderAdd);
-// buttonSubst.addEventListener('click', renderSubst);
-// buttonClear.addEventListener('click', clear);
-
+buttonClear.addEventListener('click', clear);
