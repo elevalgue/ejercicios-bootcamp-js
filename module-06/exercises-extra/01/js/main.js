73af65ec0319265ac28ec2df9b8bcd1c700fd38c
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-02 18:58:55 +0200

    Add data

--- module-06/exercises-extra/01/js/main.js
+++ module-06/exercises-extra/01/js/main.js
@@ -23,6 +23,75 @@ console.log(myArray, 'todos los elementos del array son "Hello world"');
 const myArray2 = new Array(3).fill("Hello world");
 console.log(myArray2);
 
+
 /*************************
  *        AGENDA         *
  *************************/
+
+// Constantes
+var WORK_HOURS = [
+    "08:00 - 09:00",
+    "09:00 - 10:00",
+    "10:00 - 11:00",
+    "11:00 - 12:00",
+    "12:00 - 13:00",
+    "13:00 - 14:00",
+    "15:00 - 16:00",
+    "16:00 - 17:00"
+];
+
+// Datos
+    var myTeam = [
+    {
+        name: "María",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Pedro",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Esther",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Marcos",
+        availability: new Array(8).fill(true)
+    },
+    ];
+
+    // // Constantes
+var WORK_HOURS = [
+"08:00 - 09:00",
+"09:00 - 10:00",
+"10:00 - 11:00",
+"11:00 - 12:00",
+"12:00 - 13:00",
+"13:00 - 14:00",
+"15:00 - 16:00",
+"16:00 - 17:00"
+];
+
+// Datos
+var myTeam = [
+    {
+        name: "María",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Pedro",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Esther",
+        availability: new Array(8).fill(true)
+    },
+    {
+        name: "Marcos",
+        availability: new Array(8).fill(true)
+    },
+];
+
+// Algoritmo
+
+/*----- 1. Generación aleatoria de la disponibilidad: -----*/
