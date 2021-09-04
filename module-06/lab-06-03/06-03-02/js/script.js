8253131947b0b9046741cf3e315085f5ba54c2fe
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 00:37:54 +0200

    loop to get the coins that fit in our price

--- module-06/lab-06-03/06-03-02/js/script.js
+++ module-06/lab-06-03/06-03-02/js/script.js
@@ -8,14 +8,19 @@ const inputTotalAmount = document.querySelector('#js-total-amount');
 const inputTotalGiven = document.querySelector('#js-total-given');
 const btnCalculate = document.querySelector('#js-btn-calculate'); 
 
-// const money = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
+const money = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
 
 const calculateChange = function (price, cash) {
     const returnValue = cash - price;
     const change = [];
-    const amount = 0; 
+    let amount = 0; 
 
-    return returnValue;
+    for (let index = 0; index < money.length; index++) {
+        amount = returnValue / money[index];
+        console.log(amount, 'hola');
+        
+    }
+    return change;
 }
 
 console.log(calculateChange(200, 233));
