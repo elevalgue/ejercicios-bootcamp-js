753991ee54306e60310ff1abe1dab658095cec4a
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 01:15:49 +0200

    Add money name array

--- module-06/lab-06-03/06-03-02/js/script.js
+++ module-06/lab-06-03/06-03-02/js/script.js
@@ -7,23 +7,26 @@ const inputTotalAmount = document.querySelector('#js-total-amount');
 const inputTotalGiven = document.querySelector('#js-total-given');
 const btnCalculate = document.querySelector('#js-btn-calculate'); 
 
-const moneyTypes = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
+const moneyValue = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
+const moneyType = ['dos cientos', 'cien', 'ciencuenta', 'veinte', 'diez', 'cinco', 'dos euros', 'un euro', 'cincuenta centimos', 'veinte centimos', 'diez centimos', 'cinco centimos', 'dos centimos', 'un centimo'];
 
 const calculateChange = function (price, cash) {
-    const returnValue = cash - price;
+    let returnValue = cash - price;
     const change = [];
     let amount = 0; 
 
-    for (let index = 0; index < moneyTypes.length; index++) {
-        amount = Math.floor(returnValue / moneyTypes[index]);
+    for (let index = 0; index < moneyValue.length; index++) {
+        amount = Math.floor(returnValue / moneyValue[index]);
         
         if (amount > 0) {
-            change[moneyTypes[index]] = amount;
-            returnValue = amount % moneyTypes[index]; 
+            change[moneyType[index]] = amount;
+            returnValue = returnValue % moneyValue[index]; 
         }
     }
+
     return change;
 }
 
-console.log(calculateChange(200, 233));
-console.log(calculateChange(272, 300));
+console.log(calculateChange(250, 300));
+console.log(calculateChange(270, 300));
+console.log(calculateChange(319.7, 320));
