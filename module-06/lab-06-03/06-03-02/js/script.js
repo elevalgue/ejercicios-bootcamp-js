8bc4cd3f8241ab8f60950824700c608163938993
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 00:21:40 +0200

    Get together bills & coins

--- module-06/lab-06-03/06-03-02/js/script.js
+++ module-06/lab-06-03/06-03-02/js/script.js
@@ -8,5 +8,15 @@ const inputTotalAmount = document.querySelector('#js-total-amount');
 const inputTotalGiven = document.querySelector('#js-total-given');
 const btnCalculate = document.querySelector('#js-btn-calculate'); 
 
-const notes = [200, 100, 50, 20, 10, 5];
-const coins = [2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];  
+// const money = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
+
+const calculateChange = function (price, cash) {
+    const returnValue = cash - price;
+    const change = [];
+    const amount = 0; 
+
+    return returnValue;
+}
+
+console.log(calculateChange(200, 233));
+console.log(calculateChange(272, 300));
