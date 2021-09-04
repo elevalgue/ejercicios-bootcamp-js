db855dcdef1837fbdd1a08762e82ae33775fa0b5
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-04 11:16:13 +0200

    Generate schedule randomly

--- module-06/lab-06-03/js/main.js
+++ module-06/lab-06-03/js/main.js
@@ -7,26 +7,27 @@ console.log('Are you ready for a good time? :))');
  *       UTILIDADES       *
  *************************/
 
-/*----- 1. Crear un array vacío de longitud n: -----*/
-const emptyArray = new Array(3);
-console.log(emptyArray, 'array vacío de 3 elementos');
+// /*----- 1. Crear un array vacío de longitud n: -----*/
+// const emptyArray = new Array(3);
+// console.log(emptyArray, 'array vacío de 3 elementos');
 
-const emptyArray2 = new Array(7);
-console.log(emptyArray2, 'array vacío de 7 elementos');
+// const emptyArray2 = new Array(7);
+// console.log(emptyArray2, 'array vacío de 7 elementos');
 
-/*----- 2. Rellenar todas las posiciones de un array con el mismo valor: -----*/
-const myArray = new Array(3);
-myArray.fill('Hello world')
-console.log(myArray, 'todos los elementos del array son "Hello world"');
+// /*----- 2. Rellenar todas las posiciones de un array con el mismo valor: -----*/
+// const myArray = new Array(3);
+// myArray.fill('Hello world')
+// console.log(myArray, 'todos los elementos del array son "Hello world"');
 
-// En una sola línea
-const myArray2 = new Array(3).fill("Hello world");
-console.log(myArray2);
+// // En una sola línea
+// const myArray2 = new Array(3).fill("Hello world");
+// console.log(myArray2);
 
 
 /*************************
  *        AGENDA         *
  *************************/
+
 
 
 // Constantes
@@ -68,6 +69,22 @@ let workerName = [];
 let schedule = []; 
 
 
-/*----- 1. Generación aleatoria de la disponibilidad: -----*/
-const randomAvailability = () => Math.random() < 0.5; 
+/*----- 1. GENERATE RANDOM AVAILABILITY: -----*/
+const getRandomNumber = () => Math.random() < 0.5; 
 
+/*----- 2. GET PEOPLE AVAILABILITY: -----*/
+
+function getRandomSchedule() {
+    for (const worker of myTeam) {
+        // console.log('--------------------');
+        // console.log(`Horarios de ${worker.name}`);
+
+        for (let index = 0; index < worker.availability.length; index++) {
+            worker.availability[index] = getRandomNumber(); 
+            
+        }
+    }
+    console.log(myTeam);
+}
+
+console.log(getRandomSchedule());
