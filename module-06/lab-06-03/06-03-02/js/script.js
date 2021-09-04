f22cfedb375f3096ec16bcbd278459a184ff4e23
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-04 23:24:51 +0200

    Add selectors

--- module-06/lab-06-03/06-03-02/js/script.js
+++ module-06/lab-06-03/06-03-02/js/script.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('Lets go');
+
+// Selectors
+
+const inputTotalAmount = document.querySelector('#js-total-amount'); 
+const inputTotalGiven = document.querySelector('#js-total-given');
+const btnCalculate = document.querySelector('#js-btn-calculate'); 
+
+const notes = [200, 100, 50, 20, 10, 5];
+const coins = [2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];  
