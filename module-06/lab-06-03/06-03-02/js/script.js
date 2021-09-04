10074d1f4ac1d71c60c0e119a119a3cb7d09af74
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 00:43:55 +0200

    Update returnValue

--- module-06/lab-06-03/06-03-02/js/script.js
+++ module-06/lab-06-03/06-03-02/js/script.js
@@ -3,22 +3,24 @@
 console.log('Lets go');
 
 // Selectors
-
 const inputTotalAmount = document.querySelector('#js-total-amount'); 
 const inputTotalGiven = document.querySelector('#js-total-given');
 const btnCalculate = document.querySelector('#js-btn-calculate'); 
 
-const money = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
+const moneyTypes = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
 
 const calculateChange = function (price, cash) {
     const returnValue = cash - price;
     const change = [];
     let amount = 0; 
 
-    for (let index = 0; index < money.length; index++) {
-        amount = Math.floor(returnValue / money[index]);
-        console.log(amount, 'hola');
+    for (let index = 0; index < moneyTypes.length; index++) {
+        amount = Math.floor(returnValue / moneyTypes[index]);
         
+        if (amount > 0) {
+            change[moneyTypes[index]] = amount;
+            returnValue = amount % moneyTypes[index]; 
+        }
     }
     return change;
 }
