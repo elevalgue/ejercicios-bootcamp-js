1aab1fd0554096c60bed0f30de52cbcc7f218114
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 02:55:02 +0200

    Clean

--- module-06/lab-06-03/06-03-02/js/script.js
+++ module-06/lab-06-03/06-03-02/js/script.js
@@ -8,29 +8,31 @@ const inputTotalGiven = document.querySelector('#js-total-given');
 const btnCalculate = document.querySelector('#js-btn-calculate'); 
 
 const moneyValue = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
-const moneyType = ['dos cientos', 'cien', 'ciencuenta', 'veinte', 'diez', 'cinco', 'dos euros', 'un euro', 'cincuenta centimos', 'veinte centimos', 'diez centimos', 'cinco centimos', 'dos centimos', 'un centimo'];
+// const moneyType = ['dos cientos', 'cien', 'ciencuenta', 'veinte', 'diez', 'cinco', 'dos euros', 'un euro', 'cincuenta centimos', 'veinte centimos', 'diez centimos', 'cinco centimos', 'dos centimos', 'un centimo'];
 
 /*----- 1. GET INPUT VALUES -----*/
-const getReturnValue = () => parseInt(inputTotalGiven.value); - parseInt(inputTotalAmount.value) 
+const getReturnValue = () => parseInt(inputTotalGiven.value); - parseInt(inputTotalAmount.value);
 
 /*----- 2. CALCULATE OPTIMAL CHANGE -----*/
 const calculateChange = function () {
     let returnValue = getReturnValue();
 
-    console.log(returnValue, 'maricarmen');
+    console.log(returnValue, 'vuelta');
     const change = [];
-    let amount = 0; 
+    let amount = 0;
 
     for (let index = 0; index < moneyValue.length; index++) {
         amount = Math.floor(returnValue / moneyValue[index]);
-        
+    
         if (amount > 0) {
             change[moneyType[index]] = amount;
             returnValue = returnValue % moneyValue[index]; 
         }
     }
-console.log(change, 'hola');
+    
+    console.log(change, 'hola');
     // return change;
+    
 }
 
 btnCalculate.addEventListener('click', calculateChange); 
