05537858dfbff0ea1ea3aa26b333742f598454a9
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 12:44:58 +0200

    Create money object

--- module-06/lab-06-03/06-03-02/js/script2.js
+++ module-06/lab-06-03/06-03-02/js/script2.js
@@ -9,6 +9,65 @@ const btnCalculate = document.querySelector('#js-btn-calculate');
 const moneyValue = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
 const moneyType = ['dos cientos', 'cien', 'ciencuenta', 'veinte', 'diez', 'cinco', 'dos euros', 'un euro', 'cincuenta centimos', 'veinte centimos', 'diez centimos', 'cinco centimos', 'dos centimos', 'un centimo'];
 
+const money = [
+    {
+        name: 'dos cientos',
+        value: 200,
+    },
+    {
+        name: 'cien',
+        value: 100,
+    },
+    {
+        name: 'ciencuenta',
+        value: 50,
+    },
+    {
+        name: 'veinte',
+        value: 20,
+    },
+    {
+        name: 'diez',
+        value: 10,
+    },
+    {
+        name: 'cinco',
+        value: 5,
+    },
+    {
+        name: 'dos euros',
+        value: 2,
+    },
+    {
+        name: 'un euro',
+        value: 1,
+    },
+    {
+        name: 'cincuenta centimos',
+        value: 0.5,
+    },
+    {
+        name: 'veinte centimos',
+        value: 0.2,
+    },
+    {
+        name: 'diez centimos',
+        value: 0.1,
+    },
+    {
+        name: 'cinco centimos',
+        value: 0.05,
+    },
+    {
+        name: 'dos centimos',
+        value: 0.02,
+    },
+    {
+        name: 'un centimos',
+        value: 0.01,
+    },
+
+]
 
 /*----- 1. GET INPUT VALUES -----*/
 const getReturnValue = () => parseInt(inputTotalGiven.value) - parseInt(inputTotalAmount.value);
