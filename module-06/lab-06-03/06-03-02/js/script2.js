2e8dc93101fc7820e4109741722924a244a248c8
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 13:33:10 +0200

    Clean

--- module-06/lab-06-03/06-03-02/js/script2.js
+++ module-06/lab-06-03/06-03-02/js/script2.js
@@ -6,9 +6,6 @@ const inputTotalAmount = document.querySelector('#js-total-amount');
 const inputTotalGiven = document.querySelector('#js-total-given');
 const btnCalculate = document.querySelector('#js-btn-calculate');
 
-// const moneyValue = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
-// const moneyType = ['dos cientos', 'cien', 'ciencuenta', 'veinte', 'diez', 'cinco', 'dos euros', 'un euro', 'cincuenta centimos', 'veinte centimos', 'diez centimos', 'cinco centimos', 'dos centimos', 'un centimo'];
-
 const money = [
     {
         name: 'dos cientos',
