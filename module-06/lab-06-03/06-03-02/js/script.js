e39b8db2b5e8a951f1c84300f0fe34591f0e8deb
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 02:19:24 +0200

    Get both input values

--- module-06/lab-06-03/06-03-02/js/script.js
+++ module-06/lab-06-03/06-03-02/js/script.js
@@ -11,20 +11,25 @@ const moneyValue = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.
 const moneyType = ['dos cientos', 'cien', 'ciencuenta', 'veinte', 'diez', 'cinco', 'dos euros', 'un euro', 'cincuenta centimos', 'veinte centimos', 'diez centimos', 'cinco centimos', 'dos centimos', 'un centimo'];
 
 
-const getCashValue = function () {
-    console.log(inputTotalAmount);
-    const cashValue = parseInt(inputTotalAmount.value);
-    console.log(cashValue);
-}
+// const getCashValue = function () {
+//     console.log(inputTotalAmount);
+//     const cashValue = parseInt(inputTotalAmount.value);
+//     console.log(cashValue);
+// }
 
-const getGivenMoney = function () {
-    console.log(inputTotalAmount);
-    const totalGivenValue = parseInt(inputTotalGiven.value);
-    console.log(totalGivenValue);
-}
+// const getGivenMoney = function () {
+//     console.log(inputTotalAmount);
+//     const totalGivenValue = parseInt(inputTotalGiven.value);
+//     console.log(totalGivenValue);
+// }
 
-const calculateChange = function (price, cash) {
-    let returnValue = cash - price;
+const getReturnValue = () => parseInt(inputTotalGiven.value); - parseInt(inputTotalAmount.value) 
+
+
+const calculateChange = function () {
+    let returnValue = getReturnValue();
+
+    console.log(returnValue, 'maricarmen');
     const change = [];
     let amount = 0; 
 
@@ -36,12 +41,13 @@ const calculateChange = function (price, cash) {
             returnValue = returnValue % moneyValue[index]; 
         }
     }
-
-    return change;
+console.log(change, 'hola');
+    // return change;
 }
 
-inputTotalAmount.addEventListener('change', getCashValue);
-inputTotalGiven.addEventListener('change', getGivenMoney);
+// inputTotalAmount.addEventListener('change', getCashValue);
+// inputTotalGiven.addEventListener('change', getGivenMoney);
+btnCalculate.addEventListener('click', calculateChange); 
 
 // console.log(calculateChange(250, 300));
 // console.log(calculateChange(270, 300));
