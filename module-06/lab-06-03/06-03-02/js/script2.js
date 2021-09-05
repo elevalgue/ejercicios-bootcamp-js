5e8c7a7417a45c054527ab39e7f352e269acc999
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-05 12:31:38 +0200

    Get change

--- module-06/lab-06-03/06-03-02/js/script2.js
+++ module-06/lab-06-03/06-03-02/js/script2.js
@@ -7,6 +7,7 @@ const inputTotalGiven = document.querySelector('#js-total-given');
 const btnCalculate = document.querySelector('#js-btn-calculate');
 
 const moneyValue = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01];
+const moneyType = ['dos cientos', 'cien', 'ciencuenta', 'veinte', 'diez', 'cinco', 'dos euros', 'un euro', 'cincuenta centimos', 'veinte centimos', 'diez centimos', 'cinco centimos', 'dos centimos', 'un centimo'];
 
 
 /*----- 1. GET INPUT VALUES -----*/
@@ -21,7 +22,32 @@ function getReturnValue() {
 
     let returnValue = totalGiven - totalAmount;
     console.log(returnValue);
+    // return returnValue; 
+
+    const change = [];
+    console.log(change, 'maricarmen');
+
+    let amount = 0;
+    console.log(amount, 'hey');
+
+    for (let index = 0; index < moneyValue.length; index++) {
+        amount = Math.floor(returnValue / moneyValue[index]);
+    
+        if (amount > 0) {
+            change[moneyType[index]] = amount;
+            returnValue = returnValue % moneyValue[index]; 
+        }
+    }
+
+    console.log(change, 'hola');
+    // return change;
+
 }
+
+// function calculateChange() {
+//     console.log(returnValue, 'hola');
+    
+// }
 
 // Listener
 btnCalculate.addEventListener('click', getReturnValue); 
