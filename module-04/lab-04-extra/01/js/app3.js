2ed34f8f7267000116891f3cf73c16dca29e790d
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-06 11:44:00 +0200

    Fix bug

--- module-04/lab-04-extra/01/js/app3.js
+++ module-04/lab-04-extra/01/js/app3.js
@@ -1,5 +1,6 @@
-"use strict";
-​
+'use strict';
+console.log('Are you ready for a good time? :)');
+
 // SELECTORS
 const resultEl = document.querySelector("#js-result");
 const buttonAdd = document.querySelector("#js-add");
@@ -8,15 +9,15 @@ const buttonMult = document.querySelector("#js-multiply");
 const buttonDiv = document.querySelector("#js-divide");
 const buttonEquals = document.querySelector("#js-equals");
 const buttonClear = document.querySelector("#js-clear");
-​
+
 /*----- 1. GET INPUT VALUE -----*/
 const getInputValue = () =>
-  parseInt(document.querySelector("#js-inputNumber").value);
+    parseInt(document.querySelector("#js-inputNumber").value);
 let result = 0;
-​
+
 const setResultByDefault = () =>
-  (document.querySelector("#js-result").innerHTML = result);
-​
+    (document.querySelector("#js-result").innerHTML = result);
+
 /*----- 2. MATH OPERATIONS -----*/
 const add = () => {
   const lastResult = parseInt(document.querySelector("#js-result").innerHTML);
@@ -25,7 +26,7 @@ const add = () => {
   clear();
   return result;
 };
-​
+
 // const add = () => {
 //     if (!result) {
 //         result = parseInt(getInputValue());
@@ -35,24 +36,22 @@ const add = () => {
 //         return parseInt(result);
 //     }
 // }
-​
 
-​
 /*----- 3. DISPLAY RESULTS -----*/
 const renderAdd = (typeOperation) => (resultEl.innerHTML = add(typeOperation));
 // const renderSubst = () => resultEl.innerHTML = substract();
 const renderClear = () => (resultEl.innerHTML = clear());
-​
+
 /*----- 4. CLEAR -----*/
 const clear = () => {
   document.querySelector("#js-inputNumber").value = "";
 };
-​
+
 // LISTENERS
 buttonClear.addEventListener("click", clear);
 buttonAdd.addEventListener("click", add);
 buttonEquals.addEventListener("click", renderAdd);
 setResultByDefault();
-​
+
 // buttonSubst.addEventListener('click', substract);
 // buttonEquals.addEventListener('click', renderSubst);
