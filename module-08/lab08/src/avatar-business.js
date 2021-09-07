746731be329afb558e484c6ecac9a8d6b99055dc
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-08 01:11:11 +0200

    Change to IIFE in index

--- module-08/lab08/src/avatar-business.js
+++ module-08/lab08/src/avatar-business.js
@@ -1,16 +1,8 @@
 (function (App) {
-    App.getAvatar = function () {
+    App.getAvatar = function(user) {
         const img = document.createElement('img');
         img.src = user.avatar;
 
         return img;
     }
 })(window.App || (window.App = {}));
-
-
-// function getAvatar(user) {
-//     const img = document.createElement('img');
-//     img.src = user.avatar;
-
-//     return img;
-// }
--- module-08/lab08/src/user-business.js
+++ module-08/lab08/src/user-business.js
@@ -1,12 +1,15 @@
-function getFullName(user) {
-    return (`${user.first_name} ${user.last_name}`);
-};
+(function(App) {
+    App.getFullName = function(user) {
+        return (`${user.first_name} ${user.last_name}`)
+    };
 
-function getUserNode(user) {
-    const node = document.createElement('div');
+    App.getUserNode = function(user) {
+        
+        const node = document.createElement('div');
+        
+        node.appendChild(App.getAvatar(user));
+        node.append(`${App.getFullName(user)} ${user.email}`);
 
-    node.appendChild(getAvatar(user));
-    node.append(`${getFullName(user)} ${user.email}`);
-
-    return node; 
-};
+        return node; 
+    }
+})(window.App || (window.App = {}));
