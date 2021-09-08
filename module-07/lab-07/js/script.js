8fc546b2da7668ad511e5127103248e0c088b703
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-08 17:05:27 +0200

    Improve iban pattern

--- module-07/lab-07/js/script.js
+++ module-07/lab-07/js/script.js
@@ -16,12 +16,12 @@ const ibanValidation = ibanPattern1.test(iban1);
 
 // Caso 2
 // Permitir que se incluyan espacios en ciertas áreas, daremos por valida estás dos cadenas
-const ibanPattern2 = /(([ES]{2})\d{22})|(([A-Z]{2})\d{2}(\s\d{4}){5})/;
+const ibanPattern2 = /^(([ES]{2})\d{22})|(([A-Z]{2})\d{2}(\s\d{4}){5}$)/;
 const iban2 = ['ES6600190020961234567890', 'ES66 0019 0020 9612 3456 7890'];
 
 iban2.forEach(value => {
     console.log('<---------- CAS0 2 ----------->');
-    console.log(`Regexp matches with  ${value} ------>  , ${ibanPattern2.test(value)}`);
+    console.log(`Regexp matches with  ${value} ------>  ${ibanPattern2.test(value)}`);
     console.log('<----------------------------->');
 });
 
