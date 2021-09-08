ed1819c42beb153f53ed3f3af21af647f1588388
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-08 19:55:42 +0200

    :hammer: Exercise 2.2

--- module-07/lab-07/js/script.js
+++ module-07/lab-07/js/script.js
@@ -2,6 +2,10 @@
 
 console.log('Are you ready for a good time');
 
+/**
+ * C O M P U L S O R Y 
+ */
+
 /*----- 1. VALIDAR IBAN: -----*/
 
 // Caso 1
@@ -34,8 +38,8 @@ let separated = ibanPattern3.exec(iban3);
 
 console.log('<---------- CAS0 3 ----------->');
 console.log(separated);
-console.log("Código del pais: " + separated[1]);
-console.log("Dígito de control: " + separated[2]);
+console.log("Country CODE: " + separated[1]);
+console.log("Check DIGIT " + separated[2]);
 console.log('<----------------------------->');
 
 
@@ -43,9 +47,10 @@ console.log('<----------------------------->');
 
 // Caso 1
 // Validar una matrícula de coche moderna. Compuesta por 4 digitos y tres letras
-const platePattern = /^\d{4}(\s|-|_)?[A-Za-z]{3}$/;
-const skmfskmdf = /^\d{4}(-|\s)?[A-Z]{3}$/;
+const platePattern = /^\d{4}(-|\s)?[A-Z]{3}$/;
 const plates = ['2021 GMD', '2345-GMD', '4532BDB', '0320-AAA'];
+let separated2; 
+
 
 plates.forEach(value => {
     console.log('<---------- CAS0 1 ----------->');
@@ -56,14 +61,15 @@ plates.forEach(value => {
 // Caso 2
 // Extraer la parte númerica
 // Extraer las letras
-const numberPattern = /^(\d{4})/;
-const charactPattern = /[A-Z]{3}$/;
-
-plates.forEach(value => {
-    console.log('<---------- CAS0 2 ----------->');
-    console.log(`${value} ------>  Números  ${numberPattern.exec(value)} //  Letras  ${charactPattern.exec(value)}`);
+for (let index = 0; index < plates.length; index++) {
+    let separated2 = platePattern.exec(plates);
+    console.log("LETTERS: " + separated[1]);
+    console.log("NUMBERS: " + separated[2]);
     console.log('<----------------------------->');
-});
+}
 
-/*----- 2. VALIDAR MATRÍCULA COCHE: -----*/
+/**
+ * O P T I O N A L 
+ */
+/*----- 3. EXTRAER IMAÁGENES DE UN FICHERO HTML: -----*/
 // Extraer imágenes de un fichero HTML
