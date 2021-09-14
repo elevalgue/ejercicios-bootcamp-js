c68e1a1650c2be84efb30a22f1b34825caaff9c8
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-14 07:17:32 +0200

    Clean

--- module-09/lab09/01/src/avatar-business.js
+++
@@ -1,9 +0,0 @@
-function getAvatar(user) {
-    const img = document.createElement('img');
-    img.width = 150; 
-    img.src = user.avatar_url;
-
-    return img;
-}
-
-export { getAvatar }; 
--- module-09/lab09/01/src/data-business.js
+++
@@ -1,9 +0,0 @@
-function getUsers() {
-  return fetch('https://api.github.com/orgs/lemoncode/members')
-    .then(response => {
-      return response.json();
-    }
-  );
-}
-
-export { getUsers }; 
