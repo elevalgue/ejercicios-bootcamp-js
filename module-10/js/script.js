d7b293049acba57b7e7c8ebe3530f4d467e52435
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-16 16:30:53 +0200

    Add task 03

--- module-10/js/script.js
+++ module-10/js/script.js
@@ -19,10 +19,17 @@ console.log(object);
 const hasId = ({id}) => id ? true : false;
 console.log(hasId(object));
 
-/*----- 2. hasId -----*/
-
+/*----- 2. head -----*/
 const items = ['item1', 'item2', 'item3']
 console.log(items);
 
 const head = ([firstItem]) => firstItem;
 console.log(head(items));
+
+/*----- 3. tail -----*/
+const elements = ['elem1', 'elem2', 'elem3'];
+console.log(elements);
+
+const tail = ([ , ...restElem]) => restElem;
+console.log(tail(elements));
+
