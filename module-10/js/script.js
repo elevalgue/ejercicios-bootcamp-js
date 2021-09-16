d31d9127e2bd17f253d3ea832b791532d472cfd4
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-16 14:21:44 +0200

    Add exercise 01

--- module-10/js/script.js
+++ module-10/js/script.js
@@ -1,3 +1,20 @@
 'use strict'; 
 
 console.log('Are you ready for a good time? :))');
+
+/**
+ * C O M P U L S O R Y 
+ */
+
+/*----- 1. hasId -----*/
+const object = {
+    id: 1,
+    name: 'Pepa',
+    familyName: 'Tomate',
+    age: 37,
+}
+
+const hasId = ({id}) => id ? true : false;
+console.log(hasId(object));
+
+/*----- 2. hasId -----*/
