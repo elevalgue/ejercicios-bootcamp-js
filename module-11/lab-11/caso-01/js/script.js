a5a556dad2626eea42970adddf2c3ca32b1e7159
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-20 17:30:31 +0200

    Set Class properties

--- module-11/lab-11/caso-01/js/script.js
+++ module-11/lab-11/caso-01/js/script.js
@@ -17,3 +17,28 @@ const reservas = [
         noches: 1
     }
 ];
+
+
+/**
+ * Queremos calcular: 
+ * - El subtotal de compra (precio sin IVA).
+ * - El total de reservas que ha hecho un cliente. 
+ */
+
+class HotelBooking {
+    constructor() {
+        this._booking = [];
+        this._standard = 100;
+        this._suite = 150;
+        this._vat = 0.21;
+        this._extraCharge = 40;
+        this._subtotal = 0; 
+    }
+    
+    // Aún no he añadido el 21% de IVA ni los gastos adicionales por persona extra
+    calculateSubtotal(){
+        this._subtotal = this._booking.reduce((acc, { nights, roomType }) =>
+        acc + (nights * roomType), 0);
+    };
+};
+
