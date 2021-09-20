cfe05bce85f53bc9de9451aae29f3ae578fa34b1
elena <elena.valenzuela.guerrero@gmail.com>
2021-09-20 20:19:44 +0200

    Copy instance 01 info  in 02

--- module-11/lab-11/instance-02/js/script.js
+++ module-11/lab-11/instance-02/js/script.js
@@ -1,19 +1,78 @@
-console.log('Are you ready for a good time? :))');
-
-const reservas = [
+// Data
+const bookingList = [
     {
-        tipoHabitacion: "standard",
+        typeRoom: "standard",
         pax: 1,
-        noches: 3
+        nights: 3
     },
     {
-        tipoHabitacion: "standard",
+        typeRoom: "standard",
         pax: 1,
-        noches: 4
+        nights: 4
     },
     {
-        tipoHabitacion: "suite",
+        typeRoom: "suite",
         pax: 2,
-        noches: 1
+        nights: 1
     }
 ];
+
+class CustomerBooking {
+    constructor() {
+        this._booking = [];
+        this._subtotal = 0;
+        this._total = 0;
+    }
+    
+
+    getRoomType(type) {
+        switch (type) {
+            case 'standard':
+                return 100;
+            
+            case 'suite':
+                return 150;
+        }
+    }
+
+    calculateExtraCharge(pax, nights) {
+        return (pax > 1) ? (pax * 40 * nights) : 0; 
+    }
+
+    calculateSubtotal() {
+        this._subtotal = this._booking.reduce(
+            (acc, { typeRoom, pax, nights }) =>
+                acc + (nights * this.getRoomType(typeRoom)) + this.calculateExtraCharge(pax, nights), 0)
+    }
+
+    calculateTotal() {
+        const vat = 1.21;
+        this._total = this._subtotal * vat;
+
+    }
+    
+    get subtotal() {
+        return this._subtotal; 
+    }
+    
+    get total() {
+        return this._total; 
+    }
+
+    set booking(bookingList) {
+        this._booking = bookingList;
+        this.calculateSubtotal();
+        this.calculateTotal(); 
+    }
+};
+
+const customerBooking = new CustomerBooking();
+customerBooking.booking = bookingList;
+
+console.log('--- RESERVA: CASO 1 ---');
+console.log(`Subtotal: ${customerBooking.subtotal} €`);
+console.log(`Total: ${customerBooking.total} €`);
+
+console.log('--- RESERVA: CASO 2 ---');
+// console.log(`Subtotal: ${customerBooking.subtotal} €`);
+// console.log(`Total: ${customerBooking.total} €`);
