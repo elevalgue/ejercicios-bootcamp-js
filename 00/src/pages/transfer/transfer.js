64a5a4ce034bc3231b771d0eb8f3547e57f358e1
elena <elena.valenzuela.guerrero@gmail.com>
2021-10-07 18:17:37 +0200

    Add getAccountList

--- 00/src/pages/transfer/transfer.js
+++ 00/src/pages/transfer/transfer.js
@@ -13,3 +13,21 @@ let transfer = {
 }
 
 const params = history.getParams();
+
+getAccountList(params.id).then(accountList => {
+    setAccountOptions(accountList, params.id);
+
+    if (!params.id) {
+    transfer = {
+        ...transfer,
+        account: "1"
+    };
+        
+    } else {
+    const id = params.id - 1
+    transfer = {
+        ...transfer,
+        account: accountList[id].id
+    };
+    }
+})
