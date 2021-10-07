00676bd3e6c635620565803c16b351deaa5bdbaf
elena <elena.valenzuela.guerrero@gmail.com>
2021-10-07 18:04:20 +0200

    Add vm

--- 00/src/pages/movements/movements.js
+++ 00/src/pages/movements/movements.js
@@ -2,32 +2,22 @@ import { getAccount} from '../account/account.api';
 import {getMovementsList} from './movements.api';
 import { onSetValues } from "../../common/helpers";
 import { addMovementRows } from './movements.helpers';
-import { mapMovementsListApiToVm} from './movements.mappers';
+import { mapMovementApiToVm} from './movements.mappers';
 import { history } from './../../core/router';
 
 const params = history.getParams();
 
 getAccount(params.id).then(account => {
-        onSetValues(account);
+  onSetValues(account);
 });
 
-// getMovementsList().then(movement => {
-//         if (movement.accountId === params.id) {
-//                 const vmMovementsList = mapMovementsApiToVm(movement);
-//                 addMovementRows(vmMovementsList)
-//         } else {
-//                 const vmMovementsList = mapMovementsApiToVm(movement.accountId === params.id);
-//                 addMovementRows(vmMovementsList)       
-//         }
-
-// });
 
 getMovementsList().then(movement => {
   if (movement.accountId === params.id) {
-//     const vmMovements = mapMovementsApiToVm(movement);
+    const vmMovements = mapMovementApiToVm(movement);
     addMovementRows(vmMovements);
   } else {
-//     const vmMovements = mapMovementsApiToVm(movement).filter(movement => movement.accountId === params.id );
+    const vmMovements = mapMovementApiToVm(movement).filter(movement => movement.accountId === params.id );
     addMovementRows(vmMovements);
   }
 });
