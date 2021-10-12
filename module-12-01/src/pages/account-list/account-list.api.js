e15026758e42ec0982ef32775691fdf5489dfd28
elena valenzuela <elena.valenzuela.guerrero@gmail.com>
2021-10-12 14:29:58 +0200

    Update account-list.api.js

--- module-12-01/src/pages/account-list/account-list.api.js
+++ module-12-01/src/pages/account-list/account-list.api.js
@@ -1,5 +1,4 @@
-
-    import Axios from 'axios';
+import Axios from 'axios';
     const url = `${process.env.BASE_API_URL}/account-list`;
 
     export const getAccountList = () => Axios.get(url).then(({ data }) =>
